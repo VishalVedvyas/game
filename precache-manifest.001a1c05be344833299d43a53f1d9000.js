a5c5c12142a5e8bc3540e31d44f4732dc77ab314
VishalVedvyas <vishalvedvyas@gmail.com>
2019-02-05 12:51:27 +0530

    Updates

---
+++ precache-manifest.001a1c05be344833299d43a53f1d9000.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dffb2a4fcbf3ea7182df",
+    "url": "/game/static/js/runtime~main.dffb2a4f.js"
+  },
+  {
+    "revision": "24451a58547e8817e149",
+    "url": "/game/static/js/main.24451a58.chunk.js"
+  },
+  {
+    "revision": "6ac437b1d5df840156bf",
+    "url": "/game/static/js/1.6ac437b1.chunk.js"
+  },
+  {
+    "revision": "24451a58547e8817e149",
+    "url": "/game/static/css/main.2b7d01d5.chunk.css"
+  },
+  {
+    "revision": "6ac437b1d5df840156bf",
+    "url": "/game/static/css/1.601c33fc.chunk.css"
+  },
+  {
+    "revision": "b6ca8517e0fa687ffb7d2245c579f395",
+    "url": "/game/index.html"
+  }
+];
